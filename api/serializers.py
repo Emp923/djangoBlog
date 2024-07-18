bc6ab5ede4de2c44b06189c0a230d6b2709c095a
Eric Penrod <emp923@gmail.com>
2024-07-18 11:51:40 -0400

    adds User endpoint and PUT method for Post

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,16 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from api.models import Post
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
--- api/views.py
+++ api/views.py
@@ -1,9 +1,36 @@
+from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
 from api.models import Post
-from api.serializers import PostSerializer
+from api.serializers import PostSerializer, UserSerializer
+
+
+@csrf_exempt
+def user_list(request):
+    """
+    List all users
+    """
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+
+@csrf_exempt
+def create_user(request):
+    """
+    Create a new user
+    """
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+    return JsonResponse({}, status=400)
 
 
 @csrf_exempt
@@ -40,4 +67,15 @@ def post_detail(request, id):
     post = Post.objects.get(id=id)
     if not post:
         return JsonResponse({}, status=404)
-    return JsonResponse({'post': post}, status=200)
+
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(post, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'GET':
+        return JsonResponse({'post': post}, status=200)
+
+    return JsonResponse({}, status=400)
