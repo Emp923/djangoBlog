4380eb80833d997493ba80650f91c5fd41d77eea
Eric Penrod <emp923@gmail.com>
2024-07-18 12:08:45 -0400

    adds trailing slashes to urls

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 import api.views as views
 
 urlpatterns = [
-    path('users', views.user_list, name='user_list'),
-    path('users/new', views.create_user, name='user_new'),
-    path('posts', views.post_list, name='post_list'),
-    path('posts/new', views.create_post, name='post_new'),
-    path('posts/<int:id>', views.post_detail, name='post_detail'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/new/', views.create_user, name='user_new'),
+    path('posts/', views.post_list, name='post_list'),
+    path('posts/new/', views.create_post, name='post_new'),
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
 ]
